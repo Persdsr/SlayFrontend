f087cee3c4a20812c6de892a1a566287cf6efe29
mrabd <reixter@bk.ru>
2025-02-18 14:21:31 +0500

    password input pattern fix

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -264,7 +264,7 @@ const Title = () => {
                           message: "Password must be at least 8 characters"
                         },
                         pattern: {
-                          value: /^[a-zA-Z0-9!@#$%^&*(),.?":{}|<>-]+$/,
+                          value: /^[a-zA-Z0-9!@#$%^&*(),.?":{}|\/<>-]+$/,
                           message: "Password must contain only Latin letters, numbers, and allowed special characters",
                         },
                       }
